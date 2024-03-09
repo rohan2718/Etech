d38e676cdb0fa76f4bdc86769806b3d05673672a
Jitesh38 <jp2112003@gmial.com>
2024-03-09 17:06:38 +0530

    first commit

---
+++ EtechApp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def home(request):
+    return render(request, 'homePage.html')
+
+# def home(request):
+#     return render(request, 'index.html')
+
+# def home(request):
+#     return render(request, 'index.html')
+
+# def home(request):
+#     return render(request, 'index.html')
+
+# def home(request):
+#     return render(request, 'index.html')
+
+# def home(request):
+#     return render(request, 'index.html')
+
+# def home(request):
+#     return render(request, 'index.html')
